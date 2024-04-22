3151ee418f3ba68b51c89474592cf1ed592e23b7
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-04-22 12:59:52 +0300

    Update preprocess.py

---
+++ preprocess.py
@@ -0,0 +1,14 @@
+# Major Tasks in Data Preprocessing
+# Data cleaning: to get rid of bad data
+# – smooth noisy data, fill in missing values,, identify or remove outliers,
+# and resolve duplication/inconsistencies
+# Data integration
+# – Integration of data from multiple databases, data warehouses, or files
+# – Mainly handling redundancy
+# Data reduction
+# – Dimensionality reduction/ feature selection
+# – Numerosity/size reduction
+# – Data compression (usually for multimedia data mining)
+# Data transformation
+# – Normalization
+# – Discretization and/or Concept hierarchy generation
